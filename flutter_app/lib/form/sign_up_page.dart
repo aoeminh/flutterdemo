de78f3fb118ca4b65e4ca62de842937923a78df3
aoeminh <36764215+aoeminh@users.noreply.github.com>
2019-11-16 13:48:29 +0700

    create sign up donw

--- flutter_app/lib/form/sign_up_page.dart
+++ flutter_app/lib/form/sign_up_page.dart
@@ -8,16 +8,19 @@ class SignUppage extends StatefulWidget {
 }
 
 class _SignUppageState extends State<SignUppage> {
-  Key key = GlobalKey<FormState>();
+  final key = GlobalKey<FormState>();
   String _name;
-  TextEditingController dateController ;
+  String _email;
+  String _phone;
+
+  TextEditingController dateController;
 
   @override
   void initState() {
     super.initState();
     dateController = TextEditingController();
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,46 +34,124 @@ class _SignUppageState extends State<SignUppage> {
         child: SingleChildScrollView(
           child: Form(
             key: key,
+            autovalidate: true,
             child: Column(
               //name
               children: <Widget>[
-                TextFieldSignUp(
-                  prefixIcon: Icon(Icons.person),
-                  hintText: 'Tên',
-                  onChange: (value) {
-                    _name = value;
-                  },
-                  validator: (value) {
-                    if (value.length < 6) {
-                      return 'Tên không được nhỏ hơn 6 ký tự';
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-
+                const SizedBox(height: 20),
+                _buildName(),
+                const SizedBox(height: 20),
                 //date
-                TextFieldSignUp(
-                  inputController: dateController,
-                  prefixIcon: Icon(Icons.date_range),
-                  hintText: 'Ngày sinh',
-                  inputType: TextInputType.datetime,
-                  onClick: () async {
-                    FocusScope.of(context).requestFocus(FocusNode());
-                    DateTime date = await showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime(2018),
-                        lastDate: DateTime.now());
-                    String s =
-                        new DateFormat("dd/MM/yyyy").format(date);
+                _buildDateOfBirth(),
+                const SizedBox(height: 20),
 
-                    setState(() {dateController.text =s;});
-                  },
-                ),
+                _buildEmail(),
+                const SizedBox(height: 20),
+
+                _buildPhone(),
+                const SizedBox(height: 50),
+                _buildSubmitButon()
               ],
             ),
           ),
         ),
       );
+
+  _buildName() => TextFieldSignUp(
+        prefixIcon: Icon(Icons.person),
+        hintText: 'Tên',
+        onChange: (value) {
+          _name = value;
+        },
+        validator: (value) {
+          if (value.length < 6) {
+            return 'Tên không được nhỏ hơn 6 ký tự';
+          } else {
+            return null;
+          }
+        },
+      );
+
+  _buildDateOfBirth() => TextFieldSignUp(
+        inputController: dateController,
+        prefixIcon: Icon(Icons.date_range),
+        hintText: 'Ngày sinh',
+        inputType: TextInputType.datetime,
+        onClick: () async {
+          final bottomHeight = MediaQuery.of(context).viewInsets.bottom;
+          if (bottomHeight > 0) {
+            Future.delayed(Duration(milliseconds: 1000));
+            FocusScope.of(context).requestFocus(FocusNode());
+          } else {
+            FocusScope.of(context).requestFocus(FocusNode());
+          }
+          DateTime date = await showDatePicker(
+              context: context,
+              initialDate: DateTime.now(),
+              firstDate: DateTime(2018),
+              lastDate: DateTime.now());
+          String s = new DateFormat("dd/MM/yyyy").format(date);
+
+          setState(() {
+            dateController.text = s;
+          });
+        },
+        validator: (value) {
+          if (value.length <= 0) {
+            return 'Ngày sinh không được để trống';
+          } else {
+            return null;
+          }
+        },
+      );
+
+  _buildEmail() => TextFieldSignUp(
+        hintText: 'Email',
+        prefixIcon: Icon(Icons.email),
+        onChange: (value) {
+          _email = value;
+        },
+    validator: (value) {
+      if (value.length <= 0) {
+        return 'Email không được để trống';
+      } else {
+        return null;
+      }
+    },
+      );
+
+  _buildPhone() => TextFieldSignUp(
+        hintText: 'Diện thoại',
+        inputType: TextInputType.number,
+        prefixIcon: Icon(Icons.phone),
+        onChange: (value) {
+          _phone = value;
+        },
+        validator: (value) {
+          if (value.length != 10) {
+            return 'Số điện thoại phải có 10 chữ số';
+          } else {
+            return null;
+          }
+        },
+      );
+
+  _buildSubmitButon() => InkWell(
+        onTap: _submit,
+        child: Container(
+          width: 200,
+          height: 50,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: Colors.amberAccent),
+          child: Center(child: Text('Submit')),
+        ),
+      );
+
+  _submit() {
+    if (key.currentState.validate()) {
+      key.currentState.save();
+      print('$_name \n ${dateController.text} \n $_email \n $_phone');
+    }
+  }
 }
