6cc3ed3cf69597a5fdd45c9226c73da44e40be9d
Minh <aoeminh@gmail.com>
2020-02-14 16:49:51 +0700

    create travel ui done

--- flutter_app/lib/travel_ui/activity_widget.dart
+++ flutter_app/lib/travel_ui/activity_widget.dart
@@ -5,6 +5,7 @@ class ActivityWidget extends StatefulWidget {
   final Activity activity;
 
   const ActivityWidget({Key key, this.activity}) : super(key: key);
+
   @override
   _ActivityWidgetState createState() => _ActivityWidgetState();
 }
@@ -13,19 +14,89 @@ class _ActivityWidgetState extends State<ActivityWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      margin: EdgeInsets.all(10),
+      height: 180,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           Expanded(
-            flex: 1,
-            child: Image.asset(widget.activity.imageUrl,fit: BoxFit.cover,),
+            flex: 2,
+            child: Container(
+              height: double.infinity,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    widget.activity.imageUrl,
+                    fit: BoxFit.fitHeight,
+                  )),
+            ),
           ),
           Expanded(
-            flex: 2,
-            child: Column(
-
-
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.only(left: 10, top: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    widget.activity.name,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    widget.activity.type,
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: List.generate(widget.activity.rating, (indext) {
+                      return Icon(
+                        Icons.star,
+                        color: Colors.yellow,
+                      );
+                    }),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: widget.activity.startTimes
+                        .map((item) => Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.lightBlueAccent,
+                                  borderRadius: BorderRadius.circular(15)),
+                              height: 40,
+                              width: 70,
+                              margin: EdgeInsets.only(right: 20),
+                              child: Center(
+                                child: FittedBox(
+                                  child: Text(item),
+                                ),
+                              ),
+                            ))
+                        .toList(),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.only(top: 10),
+              alignment: Alignment.topCenter,
+              child: Text(
+                '\$ ${widget.activity.price}',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
             ),
           )
         ],
--- flutter_app/lib/travel_ui/destination_detail.dart
+++ flutter_app/lib/travel_ui/destination_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/travel_ui/activity_widget.dart';
 import 'package:flutter_app/travel_ui/destination.dart';
 
 class DestinationDetail extends StatefulWidget {
@@ -14,85 +15,114 @@ class _DestinationDetailState extends State<DestinationDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Stack(
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
+      backgroundColor: Color(0xFFEEEEEE),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius:
+                        BorderRadius.vertical(bottom: Radius.circular(30)),
+                  ),
+                  child: Hero(
+                    tag: widget.destination.imageUrl,
+                    child: ClipRRect(
+                        borderRadius:
+                            BorderRadius.vertical(bottom: Radius.circular(20)),
+                        child: Image.asset(
+                          widget.destination.imageUrl,
+                          fit: BoxFit.cover,
+                        )),
+                  ),
                 ),
-                child: Hero(
-                  tag: widget.destination.imageUrl,
-                  child: ClipRRect(
-                      borderRadius:
-                          BorderRadius.vertical(bottom: Radius.circular(20)),
-                      child: Image.asset(
-                        widget.destination.imageUrl,
-                        fit: BoxFit.cover,
-                      )),
-                ),
-              ),
-              _toolbar(),
-              _info()
-            ],
-          ),
+                _toolbar(),
+                _info(),
 
-        ],
+              ],
+            ),
+            SizedBox(height: 20,),
+            _listActivity()
+          ],
+        ),
       ),
     );
   }
 
   _toolbar() => Positioned(
-    top: 30,
-    left: 10,
-    right: 10,
-    child: Container(
-      width: MediaQuery.of(context).size.width,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Icon(Icons.arrow_back),
-          Row(
-            children: <Widget>[Icon(Icons.search),
-            SizedBox(width: 10,),
-            Icon(Icons.menu)],
-          )
+        top: 30,
+        left: 10,
+        right: 10,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Icon(Icons.arrow_back),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.search),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(Icons.menu)
+                ],
+              )
+            ],
+          ),
+        ),
+      );
 
-        ],
-      ),
-    ),
-  );
-
-  _info()=> Positioned(
-    bottom: 20,
-    left: 10,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(widget.destination.city,style: TextStyle(
-          color: Colors.white,
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-        ),),
-        SizedBox(height: 10,),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
+  _info() => Positioned(
+        bottom: 20,
+        left: 10,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Icon(Icons.location_on,color: Colors.white),
-            SizedBox(width: 10,),
-            Text(widget.destination.country,style: TextStyle(
-              color: Colors.white,
-              fontSize: 15
-            ),)
+            Text(
+              widget.destination.city,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Icon(Icons.location_on, color: Colors.white),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  widget.destination.country,
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                )
+              ],
+            )
           ],
-        )
+        ),
+      );
 
+  _listActivity() => Container(
 
-      ],
+    child: ListView.builder(
 
-    ),
+        shrinkWrap: true,
+        physics: ScrollPhysics(),
+        itemCount: activities.length,
+        itemBuilder: (BuildContext context, int index){
+          return ActivityWidget(activity: activities[index],
+
+          );
+
+    }),
   );
+
 }
